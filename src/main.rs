96ee764aa2ec2f34a4e1356b00e05d39610ff80b
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2016-02-09 16:14:15 +0100

    Basic and ugly implementation of user DB CRUD operations

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,44 @@ extern crate foxbox_users;
 extern crate iron;
 extern crate router;
 
-use foxbox_users::users_router::{UsersRouter};
+use foxbox_users::users_db::{User, UsersDb};
+use foxbox_users::users_router::UsersRouter;
 use iron::prelude::*;
 use router::Router;
 
 fn main() {
+    let db = UsersDb::new();
+    let user = User {
+        id: None,
+        name: "Peter".to_string(),
+        email: "peter@domain.org".to_string()
+    };
+    println!("Creating user {:?}", user);
+    match db.create(&user) {
+        Ok(_) => println!("Yay!"),
+        Err(err) => println!("Crap {}", err)
+    }
+    match db.read() {
+        Ok(users) => {
+            println!("Users {:?}", users);
+            let mut user = users[0].clone();
+            user.name = "Pedro".to_string();
+            db.update(&user).unwrap();
+        },
+        Err(err) => println!("Crap {}", err)
+    }
+    match db.read() {
+        Ok(users) => {
+            println!("Users {:?}", users);
+            db.delete(users[0].id.unwrap());
+        },
+        Err(err) => println!("Crap {}", err)
+    }
+    match db.read() {
+        Ok(users) => println!("Users {:?}", users),
+        Err(err) => println!("Crap {}", err)
+    }
     let mut router = Router::new();
-
     UsersRouter::start(&mut router);
-
     Iron::new(router).http("localhost:3000").unwrap();
 }
--- src/users_db.rs
+++ src/users_db.rs
@@ -2,45 +2,65 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+extern crate libc;
 extern crate rusqlite;
-extern crate uuid;
 
+use self::libc::{c_int};
 use self::rusqlite::Connection;
-use self::uuid::Uuid;
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct User {
-    id: Option<i32>,
-    name: String,
-    email: String
+    pub id: Option<i32>,
+    pub name: String,
+    pub email: String
 }
 
-pub struct UserDb {
+pub struct UsersDb {
     connection: Connection
 }
 
-#[derive(Debug)]
-pub enum UserDbError {
-    Unknown
-}
+impl UsersDb {
+    pub fn new() -> UsersDb {
+        let connection = Connection::open_in_memory().unwrap();
+        connection.execute("CREATE TABLE IF NOT EXISTS users (
+                id      INTEGER PRIMARY KEY AUTOINCREMENT,
+                name    TEXT NOT NULL,
+                email   TEXT NOT NULL
+            )", &[]).unwrap();
 
-pub type UserDbResult = Result<(), UserDbError>;
-
-impl UserDb {
-    pub fn new() -> UserDb {
-        UserDb {
-            connection: Connection::open_in_memory().unwrap()
+        UsersDb {
+            connection: connection
         }
     }
 
-    pub fn create(&self, user: &User) -> UserDbResult {
-        let uuid = Uuid::new_v4();
-        self.connection.execute("INSERT INTO users (
-            id      INTEGER PRIMARY KEY,
-            name    TEXT NOT NULL,
-            email   TEXT NOT NULL
-        ) VALUES ($1, $2, $3)", &[&uuid.to_urn_string(), &user.name, &user.email]).unwrap();
+    pub fn create(&self, user: &User) -> rusqlite::Result<c_int> {
+        self.connection.execute("INSERT INTO users
+            (name, email) VALUES ($1, $2)",
+            &[&user.name, &user.email]
+        )
+    }
 
-        Ok(())
+    pub fn read(&self) -> rusqlite::Result<Vec<User>> {
+        let mut stmt = try!(self.connection.prepare("SELECT * FROM users"));
+        let rows = try!(stmt.query(&[]));
+        let mut users = Vec::new();
+        for result_row in rows {
+            let row = try!(result_row);
+            users.push(User {
+                id: row.get(0),
+                name: row.get(1),
+                email: row.get(2)
+            });
+        }
+        Ok(users)
+    }
+
+    pub fn update(&self, user: &User) -> rusqlite::Result<c_int> {
+        self.connection.execute("UPDATE users SET name=$1, email=$2
+            WHERE id=$3", &[&user.name, &user.email, &user.id])
+    }
+
+    pub fn delete(&self, id: i32) -> rusqlite::Result<c_int> {
+        self.connection.execute("DELETE FROM users WHERE id=$1", &[&id])
     }
 }
