d05a1471d0df4cd7f554ee1bcbbd9190633dea7c
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2016-03-11 17:01:23 +0100

    Issue #68. Support secure browser resources. r=francisco

--- src/lib.rs
+++ src/lib.rs
@@ -13,13 +13,14 @@ extern crate iron_test;
 
 extern crate crypto;
 extern crate iron;
+extern crate jwt;
 extern crate libc;
+extern crate rand;
 extern crate router;
 extern crate rustc_serialize;
 extern crate rusqlite;
 extern crate unicase;
-extern crate jwt;
-extern crate rand;
+extern crate urlencoded;
 
 pub mod users_db;
 pub mod users_router;
